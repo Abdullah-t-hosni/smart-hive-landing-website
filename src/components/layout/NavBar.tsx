935ff964857d53887b9132ba9a997fef90dfb869
Abdallah <abdullah.taha574@gmail.com>
2025-05-18 19:52:30 +0300

    Refactor import paths in NavBar and About components for consistency

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { useContent } from "../context/ContentfulProvider.tsx";
+import { useContent } from "../../context/ContentfulProvider";
 
 const NavBar: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import { useContent } from "../context/ContentfulProvider";
